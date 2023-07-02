d0179cf40cba66cf16be5b7458e6e4c96e468ad8
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-02 22:57:09 +0800

    add InfoBar

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -1,11 +1,15 @@
 <template>
-    <div class="w-full max-w-[100vh] bg-white aspect-square flex">
-        <Board class="m-auto"></Board>
+    <div class="flex flex-wrap">
+        <Board></Board>
+        <div class="w-full max-w-[100vmin]  bg-white">
+            <InfoBar v-for="i in 4"></InfoBar>
+        </div>
     </div>
 </template>
 
 <script setup>
 import Board from "./Game/Board.vue";
+import InfoBar from "./Game/InfoBar.vue";
 
 </script>
 
