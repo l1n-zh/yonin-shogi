5807fb1721597e26a8a5eb507013a04710a327fc
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-05 13:22:53 +0800

    add PieceStand.vue

--- src/components/Game/InfoBar.vue
+++ src/components/Game/InfoBar.vue
@@ -1,7 +1,18 @@
 <template>
-    <div class="p-[2vmin] flex w-full border">
+    <div class="flex w-full border-[0.3vmin] h-[10vmin]">
         <div class="bg-gray-300 flex-grow mr-[2vmin]">Ray</div>
         <div class="bg-green-300 mr-[2vmin]">179:59</div>
-        <div class="bg-red-400"></div>
+        <PieceStand :piecesInHand="piecesInHand"></PieceStand>
     </div>
 </template>
+
+
+<script setup>
+import PieceStand from './PieceStand.vue'
+import { reactive } from 'vue';
+
+const piecesInHand = reactive([0,0,0,0])
+</script>
+
+<style>
+</style>
---
+++ src/components/Game/PieceStand.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="bg-red-400  flex flex-row">
+        <div v-for="(number, index) in props.piecesInHand" class="relative h-full" @click="selectPiece(index)">
+            <div class="absolute text-[3vmin] shadow-white top-0 right-[10%] z-10 text-stroke"> {{ number }} </div>
+            <Piece :type="index" :facing="0" class="h-full"></Piece>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Piece from './Piece.vue'
+
+const props = defineProps({
+    piecesInHand: Array
+})
+</script>
+<style>
+.text-stroke {
+    text-shadow: -1px -1px 0 #ffffff, 1px -1px 0 #ffffff, -1px 1px 0 #ffffff, 1px 1px 0 #ffffff;
+}
+</style>
