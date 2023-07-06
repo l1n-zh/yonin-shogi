f70f4dfb48d39cde9bf9c874fc6d854c61daf862
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-06 12:05:15 +0800

    style: rename "data" to "piece"

--- src/components/Game/Square.vue
+++ src/components/Game/Square.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="border-[0.1vmin] border-black flex relative">
         <div class="absolute w-full h-full">
-            <Piece v-if="props.data.id != -1" class="m-auto w-full h-full" :facing="props.data.owner" :type="props.data.id"></Piece>
+            <Piece v-if="props.piece.type != -1" class="m-auto w-full h-full" :facing="props.piece.facing" :type="props.piece.type"></Piece>
         </div>
     </div>
 </template>
@@ -10,6 +10,6 @@
 import Piece from "./Piece.vue"
 
 const props = defineProps({
-    data: Object
+    piece: Object
 })
 </script>
