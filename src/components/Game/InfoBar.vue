1d1d36c0f8cca9ecb7fa5b75d499669029cbe6cb
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-06 12:04:23 +0800

    feat: add "facing" property

--- src/components/Game/InfoBar.vue
+++ src/components/Game/InfoBar.vue
@@ -2,7 +2,7 @@
     <div class="flex w-full border-[0.3vmin] h-[10vmin]">
         <div class="bg-gray-300 flex-grow mr-[2vmin]">Ray</div>
         <div class="bg-green-300 mr-[2vmin]">179:59</div>
-        <PieceStand :piecesInHand="piecesInHand"></PieceStand>
+        <PieceStand :piecesInHand="piecesInHand" :facing="props.facing"></PieceStand>
     </div>
 </template>
 
@@ -11,6 +11,9 @@
 import PieceStand from './PieceStand.vue'
 import { reactive } from 'vue';
 
+const props = defineProps({
+    facing: Number
+})
 const piecesInHand = reactive([0,0,0,0])
 </script>
 
