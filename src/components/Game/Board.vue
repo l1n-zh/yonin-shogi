0cdd89c1bc31814722acd530d33fbd9b030b931a
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-06 11:59:47 +0800

    feat: drop piece

--- src/components/Game/Board.vue
+++ src/components/Game/Board.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="relative background w-[100vmin] h-[100vmin]">
         <div class="absolute w-full h-[2%] px-[2%] flex justify-around">
-            <text class="text-[3vmin] leading-[2vmin] scale-50" v-for="i in 9">{{ 10-i }}</text>
+            <text class="text-[3vmin] leading-[2vmin] scale-50" v-for="i in 9">{{ 10 - i }}</text>
         </div>
         <div class="absolute w-[2%] py-[2%] h-full right-0 flex flex-col justify-around">
             <text class="text-[3vmin] leading-none scale-50 m-auto" v-for="i in 9">{{ i }}</text>
         </div>
         <div class="m-[2%] w-[96%] h-[96%] grid border-[0.1vmin] border-black">
             <div class="grid grid-cols-9" v-for="i in 9">
-                <Square v-for="j in 9" :data="board[i-1][j-1]" @click="selectSquare(i-1, j-1)"></Square>
+                <Square v-for="j in 9" :piece="board[i - 1][j - 1]" @click="selectSquare(i - 1, j - 1)"></Square>
             </div>
         </div>
     </div>
@@ -17,28 +17,27 @@
 <script setup>
 import { ref, reactive } from 'vue';
 import Square from './Square.vue'
-import { createShogiBoard, move } from './shogi'
+import { useGameStore } from '../../stores/game'
+import { move } from './socket'
 
-const board = reactive(createShogiBoard())
-let squareSelected = [-1,-1]
+const { board, selection, select, deselect, isSelected } = useGameStore();
+
+
 function selectSquare(x, y) {
-    const [selectedX, selectedY] = squareSelected;
-    if (selectedX == -1 && selectedY == -1) {
-        squareSelected = [x, y]
-    } else if(x == selectedX && y == selectedY){
-        squareSelected = [-1,-1]
+    if (!isSelected()) {
+        select(x, y)
+    } else if (x == selection.x && y == selection.y) {
+        deselect()
     } else {
-        move(board, squareSelected, [x, y]);
-        squareSelected = [-1, -1]
+        move([selection.x, selection.y], [x, y],selection.dropPiece?.type, 0);
+        deselect()
     }
 }
 
 </script>
 
 <style lang="postcss">
-.background{
-    @apply
-    bg-[url("https://pic.616pic.com/ys_img/00/09/26/HdlzqX0hmc.jpg")]
-    bg-cover
+.background {
+    @apply bg-[url("https://pic.616pic.com/ys_img/00/09/26/HdlzqX0hmc.jpg")] bg-cover
 }
 </style>
--- src/components/Game/PieceStand.vue
+++ src/components/Game/PieceStand.vue
@@ -1,19 +1,30 @@
 <template>
     <div class="bg-red-400  flex flex-row">
-        <div v-for="(number, index) in props.piecesInHand" class="relative h-full" @click="selectPiece(index)">
+        <div v-for="(number, type) in props.piecesInHand" class="relative h-full" @click="selectPiece(type)">
             <div class="absolute text-[3vmin] shadow-white top-0 right-[10%] z-10 text-stroke"> {{ number }} </div>
-            <Piece :type="index" :facing="0" class="h-full"></Piece>
+            <PieceVue :type="type" :facing="0" class="h-full" ></PieceVue>
         </div>
     </div>
 </template>
 
 <script setup>
-import Piece from './Piece.vue'
+import PieceVue from './Piece.vue'
+import { useGameStore } from '../../stores/game'
+import { Piece } from './shogi'
 
 const props = defineProps({
+    facing: Number,
     piecesInHand: Array
 })
+const { select, deselect } = useGameStore();
+
+function selectPiece(type) {
+    deselect();
+    select(-1, -1, new Piece(type, props.facing))
+}
+
 </script>
+
 <style>
 .text-stroke {
     text-shadow: -1px -1px 0 #ffffff, 1px -1px 0 #ffffff, -1px 1px 0 #ffffff, 1px 1px 0 #ffffff;
