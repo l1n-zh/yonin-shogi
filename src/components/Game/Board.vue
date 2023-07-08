2eb6a48f1370379eecae9a4729c98651d933a72a
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-08 18:11:59 +0800

    feat: 自訂格線顏色

--- src/components/Game/Board.vue
+++ src/components/Game/Board.vue
@@ -6,16 +6,16 @@
         <div class="absolute w-[2%] py-[2%] h-full right-0 flex flex-col justify-around">
             <text class="text-[3vmin] leading-none scale-50 m-auto" v-for="i in 9">{{ i }}</text>
         </div>
-        <div class="m-[2%] w-[96%] h-[96%] grid border-[0.1vmin] border-black">
+        <div class="m-[2%] w-[96%] h-[96%] grid border-[0.1vmin]" :style="{borderColor:boardConfig.gridColor}">
             <div class="grid grid-cols-9" v-for="i in 9">
                 <div class="relative" v-for="j in 9">
                     <Square class="w-full h-full" :piece="board[i - 1][j - 1]" @click="selectSquare(i - 1, j - 1)"
                         :selected="(i - 1) == selection.x && (j - 1) == selection.y"> </Square>
                     <div v-if="(i - 1) == promotionDialog.x && (j - 1) == promotionDialog.y" class="promotionDialog"
                         :style="{ transform: `rotate(${90*promotionDialog.facing}deg) ` }">
-                        <Piece :facing="promotionDialog.facing" :type="[5, 6, -1, 7, -1, -1, -1, -1][promotionDialog.type]"
+                        <Piece :facing="promotionDialog.facing" :type="[5, 6, -1, 7, -1, 5, 6, 7][promotionDialog.type]"
                             :selected="false" @click="promotionDialog.confirm(true)"></Piece>
-                        <Piece :facing="promotionDialog.facing" :type="promotionDialog.type" :selected="false"
+                            <Piece :facing="promotionDialog.facing" :type="[0, 1, 2, 3, 4, 0, 1, 3][promotionDialog.type]" :selected=" false"
                             @click="promotionDialog.confirm(false)"></Piece>
                     </div>
                 </div>
@@ -29,10 +29,13 @@ import { ref, reactive } from 'vue';
 import Square from './Square.vue'
 import Piece from './Piece.vue';
 import { useGameStore } from '../../stores/game'
+import { useConfigStore } from '../../stores/config'
 import { drop, move } from './socket'
 import { canPromote } from './shogi';
 
+
 const { board, selection, select, deselect, isSelected } = useGameStore();
+const { boardConfig } = useConfigStore();
 
 const promotionDialog = reactive({ x: -1, y: -1, facing: -1, type: -1, confirm: () => { } })
 
@@ -68,9 +71,6 @@ function selectSquare(x, y) {
     }
 }
 
-function createDialog(x, y) {
-    return
-}
 
 </script>
 
--- src/components/Game/Square.vue
+++ src/components/Game/Square.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="border-[0.1vmin] border-black relative select-none">
+    <div class="border-[0.1vmin] relative select-none" :style="{borderColor:boardConfig.gridColor}">
         <div class="absolute w-full h-full" :style="{ transform: `rotate(${90 * props.piece.facing}deg)` }">
             <div class="absolute w-full h-full bg-white bg-opacity-30 z-10" v-if="selected"></div>
-            <Piece v-if="props.piece.type != -1" class="absolute w-full h-full z-20"
-                :facing="props.piece.facing"
-                :selected="selected"
-                :type="props.piece.type"></Piece>
+            <Piece v-if="props.piece.type != -1" class="absolute w-full h-full z-20" :facing="props.piece.facing"
+                :selected="selected" :type="props.piece.type"></Piece>
         </div>
     </div>
 </template>
 
 <script setup>
 import Piece from "./Piece.vue"
+import { useConfigStore } from "../../stores/config";
+
+const { boardConfig } = useConfigStore();
 
 const props = defineProps({
     piece: Object,
@@ -19,5 +20,4 @@ const props = defineProps({
 })
 </script>
 
-<style>
-</style>
+<style></style>
