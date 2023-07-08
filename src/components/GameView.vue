dd4321fd9082388d2505034da1ccc52ca583ea4f
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-09 01:45:49 +0800

    feat: Dashboard

--- src/components/GameView.vue
+++ src/components/GameView.vue
@@ -4,6 +4,7 @@
         <div class="w-full max-w-[100vmin]  bg-white">
             <InfoBar v-for="i in 4" :facing="i - 1"></InfoBar>
             <button class="border m-[10px]" @click="startGame">new game</button>
+            <Dashboard></Dashboard>
         </div>
     </div>
 </template>
@@ -15,6 +16,7 @@ import InfoBar from "./Game/InfoBar.vue";
 
 import { socket } from "./Game/socket"
 import { useGameStore } from "../stores/game"
+import Dashboard from './Dashboard.vue';
 
 const { viewer } = useGameStore()
 viewer.facing = 0
