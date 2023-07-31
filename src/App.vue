db170c5eace4888a190da516fab72a3b8c476c4b
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-31 10:17:11 +0800

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 
 <template>
   <div class="bg-slate-600 w-[100vw] h-[100vh]">
-    <GameView/>
+    <RouterView></RouterView>
   </div>
 </template>
 
 <script setup>
-import GameView from "./components/GameView.vue";
 </script>
 
 <style></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import App from './App.vue'
 import './index.css'
+
+import GameView  from './components/GameView.vue'
+import HomeView from './components/HomeView.vue'
+import LobbyView from './components/LobbyView.vue'
+
+
+const routes = [
+    { path: '/yonin-shogi/', component: HomeView },
+    { path: '/yonin-shogi/lobby', component: LobbyView },
+    { path: '/yonin-shogi/game', component: GameView }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
 
 const pinia = createPinia()
 const app = createApp(App)
 
+app.use(router)
 app.use(pinia)
 app.mount('#app')
