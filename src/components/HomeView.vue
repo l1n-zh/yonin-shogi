a87b39cf2549920f950c403f8b48af25832967ce
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-07-31 10:17:29 +0800

    add HomeView

---
+++ src/components/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex h-full w-full">
+        <div class="flex flex-col m-auto">
+            <label class="text-white text-[10vmin]">四人將棋</label>
+            <label class="text-white text-[8vmin]">yonin-shogi</label>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
