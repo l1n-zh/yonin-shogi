264de3b37d9d56db0ebd6113ac578f9feee7de62
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-10-31 23:18:39 +0800

    add mdi iconsets

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,11 +2,22 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
-import { nordDark} from '../components/ui/theme'
+import '@fortawesome/fontawesome-free/css/all.css'
+import { aliases, fa } from 'vuetify/iconsets/fa'
+import { mdi } from 'vuetify/iconsets/mdi'
+import { nordDark } from '../components/ui/theme';
 
 export const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+      mdi,
+    },
+  },
   theme: {
     themes: { nordDark },
     defaultTheme:'nordDark'
