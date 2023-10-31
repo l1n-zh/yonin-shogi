4ef07a6275e1429546971b34b831bb1971c67ac4
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-10-31 23:18:17 +0800

    add Header.vue

---
+++ src/components/ui/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container text-center flex flex-col justify-center select-none">
+        <div class="text-[rgb(var(--v-theme-text-base))] leading-none"
+            :style="{ fontSize: `${100 / props.title.length - 0.1}cqw` }">{{ props.title }}</div>
+        <div class="text-[rgb(var(--v-theme-text-muted))] leading-none"
+            :style="{ fontSize: `${100 / props.subtitle.length - 0.1}cqw` }">{{ props.subtitle }}</div>
+    </div>
+</template>
+
+
+<script setup>
+
+const props = defineProps({
+    title: String,
+    subtitle: String
+})
+
+</script>
+
+<style scoped>
+.container {
+    container-type: size;
+}
+</style>
