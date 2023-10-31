e3cb95cdbbaa7cf54d7d876a1b50e05cbdbdf71c
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-10-31 23:17:53 +0800

    update colors

--- src/components/ui/theme.js
+++ src/components/ui/theme.js
@@ -4,18 +4,29 @@ const nordDark = {
     dark: true,
     colors: {
         'text-base': '#ECEFF4',
-        'text-muted': '#81A1C1',
+        'text-muted': '#67748E',
         'text-inverted': '#272B35',
+        'background-home': '#2E3440',
+        'background-lobby': '#272B35',
+        'background-panel':'#22262F',
         'accent': '#88C0D0',
-        'accent-sub':'',
+        'accent-sub':'#81A1C1',
         'selection':'#3B4252',
         'selection-border':'#4C566A',
         'button':'#2E3440',
+        'button-muted':'#3B4252',
+        'button-inverted':'#D8DEE9',
         'button-boarder': '#434C5E',
         'icon':'#5E81AC',
-        'panel':'#22262F',
-        'panel-border':'#2E3440',
+        'border-panel':'#2E3440',
     }
-};
+    /* 
+    button-accent
+    button
+    button-inverted
+    
+    panel
+    */
 
+};
 export { nordDark }
