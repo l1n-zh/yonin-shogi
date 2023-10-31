c8b91707bc70ce104aed52788fdfe0b9413c9e3c
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-10-31 23:16:04 +0800

    add window object to global variable

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import { vuetify } from './plugins/vuetify';
 
 const pinia = createPinia()
 const app = createApp(App)
+app.config.globalProperties.window = window
 app.use(router)
 app.use(pinia)
 app.use(vuetify)
