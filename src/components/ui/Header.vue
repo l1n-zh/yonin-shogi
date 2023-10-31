e4a2e78b858b3c6f90e3a85c176ba23155544714
L1n <89698082+l1n-zh@users.noreply.github.com>
2023-10-31 23:31:44 +0800

    fix: 文字溢出

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="container text-center flex flex-col justify-center select-none">
         <div class="text-[rgb(var(--v-theme-text-base))] leading-none"
-            :style="{ fontSize: `${100 / props.title.length - 0.1}cqw` }">{{ props.title }}</div>
+            :style="{ fontSize: `${100 / props.title.length - 0.2}cqw` }">{{ props.title }}</div>
         <div class="text-[rgb(var(--v-theme-text-muted))] leading-none"
-            :style="{ fontSize: `${100 / props.subtitle.length - 0.1}cqw` }">{{ props.subtitle }}</div>
+            :style="{ fontSize: `${100 / props.subtitle.length - 0.2}cqw` }">{{ props.subtitle }}</div>
     </div>
 </template>
 
